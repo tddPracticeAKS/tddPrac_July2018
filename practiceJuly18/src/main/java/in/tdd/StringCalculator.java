c0a2d7561b78de1997a91db068694d32806d5fc3
Abhishek <abhishekash87@gmail.com>
2018-07-17 00:53:56 +0530

    Added tests and functional logic

--- practiceJuly18/src/main/java/in/tdd/StringCalculator.java
+++ practiceJuly18/src/main/java/in/tdd/StringCalculator.java
@@ -3,6 +3,7 @@ package in.tdd;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.IntBinaryOperator;
 
 import static in.tdd.OperatorFactory.getOperator;
@@ -12,16 +13,20 @@ import static java.util.Optional.ofNullable;
 public class StringCalculator {
 
     public int add(String numbers) {
-        return stream(ofNullable(numbers).orElse("0").split(","))
+           return stream(validateInput(numbers).get().split("[,\n]"))
                 .filter(element -> !element.isEmpty())
                 .filter(element -> StringUtils.isNumeric(element))
                 .mapToInt(Integer::parseInt)
                 .reduce(OperatorFactory.getOperator("+")).getAsInt();
     }
 
+    private Optional<String> validateInput(String numbers) {
+        return Optional.of(ofNullable(numbers).orElse(numbers.contains(",\n") ? "0" : numbers));
+    }
+
     public int operate(String numbers, String operator) {
         IntBinaryOperator operation = getOperator(operator);
-        return stream(ofNullable(numbers).orElse("0").split(","))
+        return stream(validateInput(numbers).orElse("0").split(","))
                 .filter(element -> !element.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .reduce(operation)
