0894ffe37b92fba8c7589b614d81c22fa1c79fa6
Kiran Bagul <kiran.r.bagul@gmail.com>
2018-07-18 10:48:38 +0530

    [Refactoring] : Removed redudant Optionals, optimised steam operations.

--- practiceJuly18/src/main/java/in/tdd/StringCalculator.java
+++ practiceJuly18/src/main/java/in/tdd/StringCalculator.java
@@ -10,46 +10,39 @@ import static java.util.Arrays.stream;
 import static java.util.Collections.emptyList;
 import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class StringCalculator {
 
     private final String VALID_SEPARATORS = "[,|\n]";
 
     public int add(String numbers) {
-        String delimiter = extractDelimiter(numbers).get();
-        Map<Boolean, List<Integer>> lists = stream(sanitize(numbers, delimiter).get().split(delimiter))
-                .filter(input -> !input.isEmpty())
+        String delimiter = extractDelimiter(numbers);
+        Map<Boolean, List<Integer>> lists = stream(sanitize(numbers, delimiter).split(delimiter))
                 .filter(NumberUtils::isNumber)
                 .map(Integer::parseInt)
-                .collect(Collectors.groupingBy(num -> num < 0));
+                .collect(partitioningBy(num -> num < 0));
         validateNegativeInput(lists.get(true));
-        return sumOf(lists.get(false));
-    }
-
-    private int sumOf(List<Integer> list) {
-        return ofNullable(list).orElseGet(() -> emptyList()).stream()
-                .mapToInt(Integer::intValue)
-                .reduce((x, y) -> x + y).orElse(0);
+        return lists.get(false).stream().collect(summingInt(x -> Integer.valueOf(x)));
     }
 
     private void validateNegativeInput(List<Integer> list) {
-        ofNullable(list).ifPresent(elements -> {
-            throw new IllegalArgumentException("Negative values : " + elements);
-        });
+        if (!list.isEmpty()) throw new IllegalArgumentException("Negative values : " + list);
     }
 
-    private Optional<String> extractDelimiter(String input) {
-        return of(ofNullable(input)
+    private String extractDelimiter(String input) {
+        return ofNullable(input)
                 .filter(in -> in.startsWith("//"))
                 .map(in -> valueOf(input.charAt(2)))
-                .orElse(","));
+                .orElse(",");
     }
 
-    private Optional<String> sanitize(String numbers, String delimiter) {
-        return of(ofNullable(numbers)
+    private String sanitize(String numbers, String delimiter) {
+        return ofNullable(numbers)
                 .map(input -> input.replaceAll(VALID_SEPARATORS, delimiter))
                 .filter(input -> !input.contains(",,"))
-                .orElseThrow(() -> new IllegalArgumentException()));
+                .orElseThrow(() -> new IllegalArgumentException());
     }
 
 }
